dda99f8a0f35238452f77ba04f4b84dec3956440
KarlsonT13 <tolentinokarlson@yahoo.com>
2025-04-07 01:55:52 +0800

    KarlsonPush1

--- WebTestExploration/UnitTest1.cs
+++ WebTestExploration/UnitTest1.cs
@@ -32,11 +32,25 @@ namespace WebTestExploration
             WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(3));
 
             // Find the username box element
-            IWebElement searchBox = wait.Until(ExpectedConditions.ElementIsVisible(By.ClassName("user-name")));
+            IWebElement userName = wait.Until(driver => driver.FindElement(By.Id("user-name")));
 
             // Input search term and press Enter
-            searchBox.SendKeys("standard_user");
-           
+            userName.SendKeys("standard_user");
+            Thread.Sleep(3000);
+
+            // Find the password box element
+            IWebElement password = wait.Until(driver => driver.FindElement(By.Id("password")));
+
+            // Input search term and press Enter
+            password.SendKeys("secret_sauce");
+            Thread.Sleep(3000);
+
+            // Login button
+            IWebElement loginButton = wait.Until(driver => driver.FindElement(By.Id("login-button")));
+            loginButton.Click();
+            Thread.Sleep(3000);
+            
+
         }
 
         [TearDown]
