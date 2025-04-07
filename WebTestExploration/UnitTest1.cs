0b71d367d89049915f0da47a671b61de426c54b4
KarlsonT13 <tolentinokarlson@yahoo.com>
2025-04-07 19:05:16 +0800

    KarlsonPush5

--- WebTestExploration/UnitTest1.cs
+++ WebTestExploration/UnitTest1.cs
@@ -5,6 +5,8 @@ using OpenQA.Selenium.Edge;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Support.UI;
 using SeleniumExtras.WaitHelpers;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.DevTools.V131.Audits;
 
 
 
@@ -14,6 +16,10 @@ namespace WebTestExploration
     {
 
         IWebDriver driver;
+        static void FreezePage(int milliseconds)
+        {
+            Thread.Sleep(milliseconds);
+        }
 
         [SetUp]
         public void Setup()
@@ -36,77 +42,66 @@ namespace WebTestExploration
 
             // Input search term and press Enter
             userName.SendKeys("standard_user");
-            Thread.Sleep(3000);
+            FreezePage(1000);
+        
 
             // Find the password box element
             IWebElement password = wait.Until(driver => driver.FindElement(By.Id("password")));
 
             // Input search term and press Enter
             password.SendKeys("secret_sauce");
-            Thread.Sleep(2000);
+            FreezePage(1000);
 
             // Login button
             IWebElement loginButton = wait.Until(driver => driver.FindElement(By.Id("login-button")));
             loginButton.Click();
-            Thread.Sleep(2000);  
+            FreezePage(1000);  
 
-            // Add to cart button
-            IWebElement addToCartButton = wait.Until(driver => driver.FindElement(By.Id("add-to-cart-sauce-labs-backpack")));
-            addToCartButton.Click();
-            Thread.Sleep(2000);
+            // react-burger-menu-btn button
+            IWebElement menubutton = wait.Until(driver => driver.FindElement(By.Id("react-burger-menu-btn")));
+            menubutton.Click();
+            FreezePage(1000);
 
-            // Cart button
-            IWebElement cartButton = wait.Until(driver => driver.FindElement(By.Id("shopping_cart_container")));
-            cartButton.Click();
-            Thread.Sleep(2000);
+            // About button
+            IWebElement aboutbutton = wait.Until(driver => driver.FindElement(By.Id("about_sidebar_link")));
+            aboutbutton.Click();
+            FreezePage(1000);
 
-            // Checkout button
-            IWebElement checkoutButton = wait.Until(driver => driver.FindElement(By.Id("checkout")));
-            checkoutButton.Click();
-            Thread.Sleep(2000);
+            // scroll down
+            Actions actions = new Actions(driver);
+            actions.SendKeys(Keys.End).Perform();
+            FreezePage(1000);
 
-            // First name box element
-            IWebElement firstName = wait.Until(driver => driver.FindElement(By.Id("first-name")));
-            // Input search term and press Enter
-            firstName.SendKeys("Burat");
-            Thread.Sleep(2000);
+            // highlight the text
+            IWebElement text = wait.Until(driver => driver.FindElement(By.LinkText("FAQs")));
+            IJavaScriptExecutor jsExecutor = (IJavaScriptExecutor)driver;
+            jsExecutor.ExecuteScript("arguments[0].style.backgroundColor = 'yellow';", text);
+            FreezePage(3000);
 
-            // Last name box element
-            IWebElement lastName = wait.Until(driver => driver.FindElement(By.Id("last-name")));
-            // Input search term and press Enter
-            lastName.SendKeys("Silog");
-            Thread.Sleep(2000);
+            // FAQs button
+            IWebElement faqsbutton = wait.Until(driver => driver.FindElement(By.LinkText("FAQs")));
+            faqsbutton.Click();
+            FreezePage(3000);
 
-            // Zip code box element
-            IWebElement zipCode = wait.Until(driver => driver.FindElement(By.Id("postal-code")));
-            // Input search term and press Enter
-            zipCode.SendKeys("696969");
-            Thread.Sleep(2000);
+            // API testing button
+            IWebElement apitestingbutton = wait.Until(driver => driver.FindElement(By.XPath("//span[contains(@class, 'MuiTypography-root') and text()='API Testing']")));
+            apitestingbutton.Click();
+            FreezePage(1000);
+            
+             // 1st FAQ button
+            IWebElement FAQ1 = wait.Until(driver => driver.FindElement(By.XPath("//h3[contains(@class, 'MuiTypography-root') and text()='Can I use saucectl CLI to write and edit API tests?']")));
+            FAQ1.Click();
+            FreezePage(1000);
 
-            // Continue button
-            IWebElement continueButton = wait.Until(driver => driver.FindElement(By.Id("continue")));
-            continueButton.Click();
-            Thread.Sleep(2000);
+             // highlight the text
+            IWebElement FAQ1text = wait.Until(driver => driver.FindElement(By.XPath("//a[contains(@class, 'MuiTypography-root') and text()='Sauce Piestry']")));
+            jsExecutor.ExecuteScript("arguments[0].style.backgroundColor = 'yellow';", FAQ1text);
+            FreezePage(3000);
 
-            // Finish button
-            IWebElement finishButton = wait.Until(driver => driver.FindElement(By.Id("finish")));
-            finishButton.Click();
-            Thread.Sleep(2000);
-
-            // Back home button
-            IWebElement backHomeButton = wait.Until(driver => driver.FindElement(By.Id("back-to-products")));
-            backHomeButton.Click();
-            Thread.Sleep(2000);
-
-            // Logout button
-            IWebElement logoutButton = wait.Until(driver => driver.FindElement(By.Id("react-burger-menu-btn")));
-            logoutButton.Click();
-            Thread.Sleep(2000);
-
-            // Logout button
-            IWebElement logoutButton2 = wait.Until(driver => driver.FindElement(By.Id("logout_sidebar_link")));
-            logoutButton2.Click();
-            Thread.Sleep(2000);
+            // FAQ1text button
+            IWebElement FAQ1textbutton = wait.Until(driver => driver.FindElement(By.XPath("//a[contains(@class, 'MuiTypography-root') and text()='Sauce Piestry']")));
+            FAQ1textbutton.Click();
+            FreezePage(5000);
 
         }
 
